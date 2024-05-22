855e6bbb8424d17fa429e1b96161ab503b413215
Suhodolov Matwey <Msuxodolov18@gmail.com>
2024-05-22 23:40:05 +0300

    4 задание 7 вариант

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,7 +10,7 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marksToAdd) {
-	this.marks = [...this.marks, ...marksToAdd];
+	this.marks ? this.marks.push(...marksToAdd) : null;
 }
 
 Student.prototype.getAverage = function() {
@@ -19,19 +19,7 @@ Student.prototype.getAverage = function() {
 
 
 Student.prototype.exclude = function(reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+	this.subject = undefined;
+	delete this.marks;
+	this.excluded = reason;
 }
-let student1 = new Student("Василиса", "женский", 19);
-student1.setSubject("Algebra");
-console.log(student1.getAverage()); // 0
-student1.addMarks(4, 5, 4, 5);
-console.log(student1.getAverage()); // 4.5
-console.log(student1);
-// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
-let student2 = new Student("Артём", "мужской", 25);
-student2.setSubject("Geometry");
-student2.exclude('плохая учёба')
-console.log(student2)
-// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
